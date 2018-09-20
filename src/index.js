109cd474de3145c6671b9ebafe776b85be0f1ac8
Knuttel, Karl <karl.knuttel@aperto.com>
2018-09-20 14:45:40 +0200

    Added simple CSS and webpack alias for components filepath. Fixed package.json issues.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Docs from './docs/Docs';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <Docs />, 
